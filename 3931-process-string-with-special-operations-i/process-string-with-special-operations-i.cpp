fce4958aa4f9c3bb690828eb9d25eed7049f74bb
alwaysphoenixR <rajveersinghofficial.cse@gmail.com>
2026-06-16 15:07:39 +0530

    Time: 11 ms (15.15%) | Memory: 39.9 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(isalpha(s[i])){
+                res+=s[i];
+            }else if(s[i]=='*'){
+                if(!res.empty())
+                res.pop_back();
+            }else if (s[i]=='#'){
+                res+=res;
+            }else{
+                reverse(res.begin(),res.end());
+            }
+        }
+        return res;
+    }
+};
