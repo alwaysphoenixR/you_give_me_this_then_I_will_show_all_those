e011325896f225e78f0c4fc0b5f34a3536759032
Rajveer <rajveersinghofficial.cse@gmail.com>
2026-06-26 17:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (65.82%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-void f1(vector<vector<int>>&ans,int i,vector<int>&nums,set<vector<int>>&s1){
-    if(i==nums.size()){
-        if(s1.count(nums))return;
-        s1.insert(nums);
-        ans.push_back(nums);
-        return ;
+void dfs(vector<vector<int>>&ans,vector<int>&temp,vector<int>&nums,unordered_map<int,int>&m1){
+    if(temp.size()==nums.size()){
+        ans.push_back(temp);
+        return;
     }
-    for(int j=i;j<nums.size();j++){
-        swap(nums[i],nums[j]);
-        f1(ans,i+1,nums,s1);
-        swap(nums[i],nums[j]); 
+    for(auto [key,val]:m1){
+        if(val==0)continue;
+            temp.push_back(key);
+            m1[key]--;
+            dfs(ans,temp,nums,m1);
+            temp.pop_back();
+            m1[key]++;
+        
     }
 }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-         vector<vector<int>>ans;
-         set<vector<int>>s1;
-        //  map<pair<int,int>,int>m1;
-        f1(ans,0,nums,s1);
+        unordered_map<int,int>m1;
+        for(int i=0;i<nums.size();i++){
+            m1[nums[i]]++;
+        }
+        vector<vector<int>>ans;
+        vector<int>temp;
+        dfs(ans,temp,nums,m1);
         return ans;
     }
 };
