cce419e44c7ea72fdeedb00712a045789a4fdb2a
Rajveer <rajveersinghofficial.cse@gmail.com>
2026-07-30 13:22:33 +0530

    Time: 3 ms (6.33%) | Memory: 15.3 MB (50.42%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -20,13 +20,28 @@ void go_thru(TreeNode* root,vector<int>&nums){
     go_thru(root->left,nums);
     go_thru(root->right,nums);
 }
+bool check_thru(TreeNode* root ,vector<int>& temp,int &idx){
+    if(root==NULL)return 1;
+    // else if(root==NULL && !temp.empty())return 0;
+    if(root->left==NULL && root->right==NULL && root->val==temp[idx]){idx++;return 1;}
+    else if (root->left==NULL && root->right==NULL && root->val!=temp[idx])return 0;
+    bool a1=check_thru(root->left,temp,idx);
+    bool a2=check_thru(root->right,temp,idx);
+    return a1&&a2;
+}
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         
         vector<int>nums;
         vector<int>temp;
      go_thru(root1,nums);
-     go_thru(root2,temp);
-     return (temp==nums);
+     for(auto i:nums){
+        cout<<i<<" ";
+     }
+     cout<<endl;
+     int i=0;
+     return check_thru(root2,nums,i) && i==nums.size();
+    //  go_thru(root2,temp);
+    //  return (temp==nums);
         
     }
 };
