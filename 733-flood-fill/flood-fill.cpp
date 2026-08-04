5ab1e0aac34b3497f54454f5bf8263830dbd853d
Rajveer <rajveersinghofficial.cse@gmail.com>
2026-08-04 22:54:49 +0530

    Time: 3 ms (12.64%) | Memory: 18.6 MB (14.10%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<int>dx={1,-1,0,0};
+        vector<int>dy={0,0,1,-1};
+        int n=image.size();
+        int m=image[0].size();
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int val=image[sr][sc];
+        while(!q.empty()){
+            auto [x,y]=q.front();
+            vis[x][y]=1;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx>=0 && nx<n && ny>=0 && ny<m && image[nx][ny]==val && !vis[nx][ny]){
+                    q.push({nx,ny});
+                }
+            }
+
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]){
+                    image[i][j]=color;
+                }
+            }
+        }
+        return image;
+    }
+};
